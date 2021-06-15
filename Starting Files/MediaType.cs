4c762630e6ff743c38baaa55070d2d82999012df
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-06-15 09:38:27 -0700

    Create MediaType.cs

---
+++ Starting Files/MediaType.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Treehouse.MediaLibrary
+{
+    class MediaType
+    {
+        public string Title;
+        public string Loanee;
+        public bool OnLoan;
+        
+        public MediaType(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new Exception("A media type must have a title.");
+            }
+            
+            Title = title;
+        }
+
+        public void Loan()
+        {
+            OnLoan = true;
+        }
+        
+        public void Loan(string loanee)
+        {
+            Loanee = loanee;
+            Loan();
+        }
+        
+        public void Return()
+        {
+            Loanee = null;
+            OnLoan = false;
+        }        
+    }
+}
