a183e2dff152f31cf5f6481388c812cf243cc83c
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-06-15 09:38:50 -0700

    Update Movie.cs

--- Starting Files/Movie.cs
+++ Starting Files/Movie.cs
@@ -1,14 +1,32 @@
 namespace Treehouse.MediaLibrary
 {
-    class Movie
+    class Movie : MediaType
     {
-        public readonly string Title;
         public readonly string Director;
         
         public Movie(string title, string director)
+            : base(title)
         {
-            Title = title;
             Director = director;
+        }
+
+        public string GetDisplayText()
+        {
+            string text = "Movie: " + Title + " by " + Director;
+            
+            if (OnLoan)
+            {
+                if (!string.IsNullOrEmpty(Loanee))
+                {
+                    text += " (Currently on loan to " + Loanee + ")";
+                }
+                else
+                {
+                    text += " (Currently on loan)";
+                }                
+            }
+            
+            return text;
         }
     }
 }
